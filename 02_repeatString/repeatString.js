93a0c4cf9614a0d422980d6d25050f98737c43da
Dimitris Livanas <livanas.dimitris@gmail.com>
2023-04-14 12:00:31 +0200

    solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
-};
+function repeatString(message, times) {
+  string = "";
+  if (times < 0) return "ERROR";
+  for (let i = 0; i < times; i++) {
+    string += message;
+  }
+  return string;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
