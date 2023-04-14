590f8ff599817a13aaa5dba680840e8e6ebe8b88
Dimitris Livanas <livanas.dimitris@gmail.com>
2023-04-14 12:50:56 +0200

    solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(message) {
+    reversedMessage = message.split("").reverse().join("");
+    return reversedMessage;
+}
 // Do not edit below this line
 module.exports = reverseString;
