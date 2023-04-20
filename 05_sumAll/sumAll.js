391b073d35520bed39e8ec363eb72b3022d1b9a8
Dimitris Livanas <livanas.dimitris@gmail.com>
2023-04-20 10:12:22 +0200

    solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+  let sum = 0;
+  if (x < 0 || y < 0 || typeof x != "number" || typeof y != "number") {
+    return "ERROR";
+  } else if (x > y) {
+    for (let i = y; i <= x; i++) {
+      sum += i;
+    }
+    return sum;
+  } else if (x < y) {
+    for (let i = x; i <= y; i++) {
+      sum += i;
+    }
+    return sum;
+  }
 };
 
 // Do not edit below this line
